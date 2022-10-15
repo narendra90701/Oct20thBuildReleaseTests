3daffd6379c93e7710d336aef1d498fcced5d8f1
naren <naren@Naidu>
2022-10-15 08:40:29 +0530

    Oct20th Build Code Tests First Commit

---
+++ src/oct20thBuildCodeTestScripts/SampleTest.java
@@ -0,0 +1,14 @@
+package oct20thBuildCodeTestScripts;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10;
+		
+		System.out.println("The value of X is : " + x);
+
+	}
+
+}
